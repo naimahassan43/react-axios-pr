1bb50d6e715994adc64db757e73e1bdc1f83a84d
Naima Hassan <naima.hassan434@gmail.com>
2022-06-02 17:38:32 +0100

    Interceptors

---
+++ src/axios/interceptors.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const authFetch = axios.create({
+  baseUrl: "https://course-api.com/",
+});
+
+authFetch.interceptors.request.use(
+  (request) => {
+    request.headers.common["Accept"] = "application/json";
+    console.log("request sent");
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(
+  (response) => {
+    console.log("got response");
+    return response;
+  },
+  (error) => {
+    console.log(error.response);
+    if(error.response.status === 404) {
+       console.log('Not Found');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default authFetch;
